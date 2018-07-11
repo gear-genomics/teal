e97ec6ebd8391b866c7118d0849ec2f02776eb82
tobiasrausch <rauschtobi@gmail.com>
2018-07-11 16:31:51 +0200

    switch to tracy

--- server/server.py
+++ server/server.py
@@ -48,13 +48,11 @@ def generate():
 
     # Run teal
     outfile = os.path.join(sf, "teal_" + uuidstr + ".json")
-    tsvfile = os.path.join(sf, "teal_" + uuidstr + ".tsv")
     logfile = os.path.join(sf, "teal_" + uuidstr + ".log")
     errfile = os.path.join(sf, "teal_" + uuidstr + ".err")
     with open(logfile, "w") as log:
         with open(errfile, "w") as err:
-            texe = os.path.join(app.config['TEAL'], "./src/teal")
-            return_code = call([texe, fexpname, outfile, tsvfile], stdout=log, stderr=err)
+            return_code = call(['tracy', 'basecall', '-o', outfile, fexpname], stdout=log, stderr=err)
     if return_code != 0:
         errInfo = "!"
         with open(errfile, "r") as err:
